07b48b580ab0989ddaa4e56e856c6301d1247312
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-02-24 22:04:26 +0300

    feat: add sollution to Carbon Dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,22 @@
+//Your task is to implement the function dateSample(sampleActivity) that calculates sample approximate age (in years). 
+//Please use given MODERN_ACTIVITY and HALF_LIFE_PERIOD.
+//Function parameter sampleActivity is a string. 
+//Calculated sample age must be number. 
+//Age must be integer. 
+//Age must be rounded up. 
+//In case of wrong input parameter type or inadequate activity value or absence of argument function must return false.
+
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  
+  let sa = parseFloat(sampleActivity);
+  
+    if((typeof(sampleActivity)!=='string')|| (sampleActivity<=0) || (sampleActivity > MODERN_ACTIVITY) || (sa!==sa) ) return false; 
+    else{
+    
+    let a = (Math.log(MODERN_ACTIVITY/sa)) * (HALF_LIFE_PERIOD/0.693); 
+     return Math.ceil(a); 
+    }  
 };
