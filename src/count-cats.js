70886ecc4a00a987082970019eea7fad891f0d76
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-02-24 16:34:46 +0300

    feat: add sollution to Count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let t = matrix.join(",").split(",").filter(i => i == '^^');
+  if (t.length !== 0) return t.length;
+  else return 0;
 };
