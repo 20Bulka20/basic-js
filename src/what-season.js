a2d58ca3605f34c4e7c2d6cf162d205c1ddbb714
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-03-08 15:39:38 +0300

    feat: add my sollution to What Season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,24 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+// What season?
+// Your task is to implement the function getSeason(date) that accepts Date object and returns the time of the year that matches it. 
+//Time of the year must be string.
+// If the date argument was not passed, the function must return the string 'Unable to determine the time of year!'. 
+//If the date argument is invalid, the function must throw an Error.
+// Shh! An enemy scout has lurked among the arguments that come into this function.
+// Disguised
+// He is guided by the famous proverb: 
+//“If it looks like a duck, swims like a duck and quacks like a duck, 
+//then it probably is a duck (who cares what it really is)”. 
+//He is expertly disguised as a real date, but a clever javascript developer can catch him and throw an Error just in time!
+
+
+module.exports = function getSeason(date) {
+  if (date == undefined) return 'Unable to determine the time of year!';
+  else if (isNaN(date)) throw new UserException("the date argument is invalid");
+  else {
+    var month = date.getMonth();
+    if (month == 11 || month == 0 || month == 1) return 'winter';
+    else if (month == 2 || month == 3 || month == 4) return 'spring';
+    else if (month == 5 || month == 6 || month == 7) return 'summer';
+    else if (month == 8 || month == 9 || month == 10) return 'fall';
+  }
 };
