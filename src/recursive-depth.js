945ddc7c5f27433485722a910cd592d6724efd07
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-03-17 10:42:29 +0300

    feat: add sollution to recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,18 @@
+// Recursive depth calculator
+// Go deeper
+// Your task is to implement the class DepthCalculator with method calculateDepth that takes an array and returns its depth.
+// calculateDepth method must pass the given array recursively. Depth of a flat array is 1. 
+//Method must correctly work with arrays that contain no elements or contain empty arrays.
+// For example:
+// const depthCalc = new DepthCalculator(); const { calculateDepth } = depthCalc;
+// calculateDepth([1, 2, 3, 4, 5]) => 1
+// calculateDepth([1, 2, 3, [4, 5]]) => 2
+// calculateDepth([[[]]]) => 3
+
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let depth = 0;
+        depth = Math.max(...arr.map(elem => Array.isArray(elem) ? this.calculateDepth(elem) : 0), depth);
+        return depth + 1;
     }
 };
