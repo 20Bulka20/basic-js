2cd701c2f70f102e5a3a1ef5624e5c53023b671e
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-03-21 16:07:04 +0300

    feat: add sollution to extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,56 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+// Extended repeater
+// Your task is to implement the function repeater(str, options). 
+//This function returns a repeating string based on the given parameters:
+// str is a string to repeat;
+// options is an object of options, that contains properties:
+// repeatTimes sets the number of repetitions of the str;
+// separator is a string separating repetitions of the str;
+// addition is an additional string that will be added to each repetition of the str;
+// additionRepeatTimes sets the number of repetitions of the addition;
+// additionSeparator is a string separating repetitions of the addition.
+// The str and addition parameters are strings by default. In case when type of these parameters is different, 
+//they must be converted to a string.
+// separator and additionSeparator parameters are strings.
+// repeatTimes and additionRepeatTimes are integer numbers (in the absence of any of them, the corresponding string is not repeated).
+// The only indispensable parameter is str, any others may be not defined. separator default value is '+'. 
+//additionSeparator default value is '|'.
+// For example: 
+//repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+// => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+module.exports = function repeater(str, options) {
+  str=str+'';
+  let count = +options.repeatTimes;
+  let custseparator = options.separator;
+  let addition = options.addition;
+  let additionRepeatTimes = +options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+
+  if (typeof addition !== 'undefined') {   
+    let rpta = str + addition;
+    if (typeof additionRepeatTimes !== 'undefined') {
+
+      for (let i = 0; i < additionRepeatTimes - 1; i++) {
+
+        if (typeof additionSeparator !== 'undefined') {
+          rpta += (additionSeparator + addition);
+        }
+        else {
+          rpta = rpta + addition;
+        }
+      }
+      str=rpta;
+    }
+    else {str=str+addition;}
+  }
+
+  let rpt = str;
+  for (let i = 0; i < count - 1; i++) {
+    if (typeof custseparator !== 'undefined') {
+      rpt += (custseparator + str);
+    }
+    else {
+      rpt += ('+' + str);
+    }
+  }
+  return rpt;
 };
-  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,83 @@
-module.exports = function transform(/* arr */) {
+// Transform array
+// Your task is to implement the function transform(arr) that takes an array and returns it transformed, 
+//based on the control sequences that it contains. Control sequences are defined string elements of the mentioned array:
+// --discard-next excludes the next element of the array from the transformed array.
+// --discard-prev excludes the previous element of the array from the transformed array.
+// --double-next doubles the next element of the array in the transformed array.
+// --double-prev doubles the previous element of the array in the transformed array.
+// For example:
+// transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
+
+// transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
+
+// Control sequences do not fall into the transformed array. Control sequences in initial array do not occur in a row. 
+//If there is no element next to the control sequence to which it can be applied, it does nothing. 
+//The function should throw an Error if the arr is not an Array.
+
+module.exports = function transform(/*arr*/) {
+
+
     throw 'Not implemented';
     // remove line with error and write your code here
+
+    // if (Array.isArray(arr) && (typeof arr) !== 'undefined') {
+
+
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if(typeof arr[i]!=='undefined'){
+    //         if (arr[i] === '--double-next') {
+    //             arr[i] = arr[i + 1];
+    //         }
+    //         else if (arr[i] === '--double-prev') {
+    //             arr[i] = arr[i - 1];
+    //         }
+    //         else if (arr[i] === '--discard-next') {
+    //             for (let k = i; k < arr.length; k++) {
+    //                 arr[k] = arr[k + 1];
+    //             }
+    //             arr.pop();
+    //         }
+    //         else if (arr[i] === '--discard-prev') {
+    //             for (let k = i; k < arr.length; k++) {
+    //                 arr[k] = arr[k - 1];
+    //             }
+    //             arr.pop();
+    //         }}
+    //     }
+    //     return arr;
+
+    //     // let a = arr.some(elem => elem === '--discard-prev' || elem === '--double-prev');    
+    //     // if (a) {
+    //     //     let rarr = arr.reverse();
+    //     //     for (let i = 0; i < rarr.length; i++) {
+    //     //         if (rarr[i] === '--double-prev') {
+    //     //             rarr[i] = rarr[i + 1];
+    //     //             return (rarr.reverse());
+    //     //         }
+    //     //         if (rarr[i] === '--discard-prev') {
+    //     //             for (let k = i; k < rarr.length; k++) {
+    //     //                 rarr[k] = rarr[k + 1];
+    //     //             }
+    //     //             rarr.pop();
+    //     //         }
+    //     //     }
+    //     //     return rarr.reverse();
+    //     // }
+    //     // else {
+    //     //     for (let i = 0; i < arr.length; i++) {
+    //     //         if (arr[i] === '--double-next') {
+    //     //             arr[i] = arr[i + 1];
+    //     //             return (arr);
+    //     //         }
+    //     //         if (arr[i] === '--discard-next') {
+    //     //             for (let k = i; k < arr.length; k++) {
+    //     //                 arr[k] = arr[k + 1];
+    //     //             }
+    //     //             arr.pop();
+    //     //         }
+    //     //     }
+    //     //     return arr;
+    //     // }
+    // }
+    // else throw new UserException("the arr is invalid");
 };
